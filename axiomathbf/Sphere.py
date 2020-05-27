857c5cc08d55976bf714ed8743d7123080ec9f35
AoWangDrexel <aw3338@drexel.edu>
2020-05-27 16:16:17 -0400

    Add format equation method

--- axiomathbf/Sphere.py
+++ axiomathbf/Sphere.py
@@ -8,6 +8,41 @@ class Sphere(Circle):
         self.eq = eq
         self.__x, self.__y, self.__z = symbols("x y z")
 
+    def isPointInSphere(self, point):
+        c1, c2, c3 = self.center
+        checkZero = self.getEquation().subs(
+            [(self.__x, c1), (self.__y, c2), (self.__z, c3)])
+        return checkZero == 0
+
+    def __completeTheSquare(self, n):
+        return (n/2)**2
+
+    def __getCenter(self, n):
+        return Point(n[0]/2, n[1]/2, n[2]/2)
+
+    def __formatEquation(self):
+        eq = self.eq.as_coefficients_dict()
+        for squaredVar in [self.__x**2, self.__y**2, self.__z**2]:
+            coeff = eq[squaredVar]
+            if coeff != 1:
+                for key in eq:
+                    eq[key] /= eq[squaredVar]
+                break
+        completedSquare = [-self.__completeTheSquare(
+            var) for var in [self.__x, self.__y, self.__z]]
+
+        constant = eq[1] + sum(completedSquare)
+        center = self.__getCenter(completedSquare)
+
+        return (self.__x+center.x)**2 + (self.__y+center.y)**2 + \
+            (self.__z-center.z)**2
+
+    def getEquation(self):
+        if eq:
+            return self.__formatEquation()
+        return (self.__x-self.center.x) + (self.__y-self.center.y) + \
+            (self.__z-self.center.z) - radius**2
+
     def getCenter(self):
         if eq:
             return 
@@ -17,35 +52,6 @@ class Sphere(Circle):
         if eq:
             return
         return self.radius
-
-    def getEquation(self):
-        return (self.__x-self.center.x) + (self.__y-self.center.y) + \
-            (self.__z-self.center.z) - radius**2
-
-    def isPointInSphere(self, point):
-        c1, c2, c3 = self.center
-        checkZero = self.getEquation().subs(
-            [(self.__x, c1), (self.__y, c2), (self.__z, c3)])
-        return checkZero == 0
-
-    def completeTheSquare(self, n):
-        return (n/2)**2
-
-    def formatEquation(self):
-        eq = self.eq.as_coefficients_dict()
-        for squaredVar in [self.__x**2, self.__y**2, self.__z**2]:
-            coeff = eq[squaredVar]
-            if coeff != 1:
-                for key in eq:
-                    eq[key] /= eq[squaredVar]
-                break
-        completedSquare = [-self.completeTheSquare(
-            var) for var in [self.__x, self.__y, self.__z]]
-
-        constant = eq[1] + sum(completedSquare)
-        return (self.__x+completedSquare[0]/2)**2 + \
-            (self.__y+completedSquare[1]/2)**2 + \
-            (self.__z-completedSquare[2]/2)**2
 
     def __str__(self):
         return str(self.getEquation())
@@ -61,4 +67,4 @@ class Sphere(Circle):
 
 
 if __name__ == "__main__":
-    pass
+    sph = Sphere()
