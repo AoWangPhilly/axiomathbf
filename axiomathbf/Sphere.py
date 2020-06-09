e0c51d38750979650544733854ac3b6cd537a009
AoWangDrexel <aw3338@drexel.edu>
2020-06-09 02:10:20 -0400

    Make Sphere formatEquation more efficient

--- axiomathbf/Sphere.py
+++ axiomathbf/Sphere.py
@@ -6,6 +6,9 @@ Date: June 08, 2020
 """
 
 from sympy import Point, Eq, simplify, factor, sqrt, symbols
+from mpl_toolkits.mplot3d import Axes3D
+import matplotlib.pyplot as plt
+import time
 
 
 class Sphere():
@@ -50,43 +53,33 @@ class Sphere():
         """
         return not self.__eq__(other)
 
-    def __completeTheSquare(self, n):
-        """A helper method that helps complete the square to format the sphere equation.
-
-        :param n: The term coefficient 
-        :type n: int
-        :returns: The term coefficient halfed and squared
-        :rtype: int
-        """
-        return (n/2)**2
-
     def __formatEquation(self):
         """Formats the equation to center-radius form
 
         :returns: The clean center-radius equation
         :type eq: sympy.core.add.Add
         """
-        self.eq = simplify(factor(self.eq))
-        eq = self.eq.as_coefficients_dict()
-        center = []
+        self.eq = factor(self.eq)
+        equationDict = self.eq.as_coefficients_dict()
+        secondOrder = []
+        for var in [x**2, y**2, z**2]:
+            if equationDict[var] != 0:
+                secondOrder.append(equationDict[var])
 
-        for squaredVar in [self.__x**2, self.__y**2, self.__z**2]:
-            coeff = eq[squaredVar]
-            if coeff == 0:
-                continue
-            if coeff != 1:
-                break
+        for idx in range(len(secondOrder)-1):
+            if secondOrder[idx] != secondOrder[idx+1]:
+                raise Exception("Nonsymmetrical, not a sphere!")
+        else:
+            for key in equationDict:
+                equationDict[key] /= secondOrder[0]
 
-        if coeff != 0:
-            for key in eq:
-                eq[key] /= coeff
+        center = Point([equationDict[var]/-2 for var in [x, y, z]])
+        radiusSquared = center.x**2 + center.y**2 + \
+            center.z**2 - equationDict[1]
 
-        radiusSquared = eq[1]
-        for var in [self.__x, self.__y, self.__z]:
-            radiusSquared -= self.__completeTheSquare(eq[var])
-            center.append(-eq[var]/2)
+        if radiusSquared < 0:
+            raise Exception("Radius is negative, not a sphere!")
 
-        center = Point(center)
         return ((self.__x-center.x)**2 + (self.__y-center.y)**2 +
                 (self.__z-center.z)**2 + radiusSquared, center, sqrt(abs(radiusSquared)))
 
@@ -102,6 +95,9 @@ class Sphere():
         checkZero = self.getEquation().subs(
             [(self.__x, c1), (self.__y, c2), (self.__z, c3)])
         return checkZero == 0
+
+    def draw(self):
+        pass
 
     def getEquation(self):
         if self.eq:
@@ -127,8 +123,6 @@ class Sphere():
 
 if __name__ == "__main__":
     x, y, z = symbols("x y z")
-    sphere1 = Sphere(eq=4*x**2 + 4*y**2 - 16*x - 24*y + 51)
-    sphere2 = Sphere(eq=(x - 2)**2 + (y - 3)**2 - 1/4)
-    print(sphere1)
-    print(sphere2)
-    print(type(4*x**2 + 4*y**2 - 16*x - 24*y + 51))
+    eq = x**2+z**2-4*x-8*z+13
+    start = time.time()
+    print(Sphere(eq=eq))
--- test/test_sphere.py
+++ test/test_sphere.py
@@ -2,18 +2,23 @@ import pytest
 from axiomathbf.Sphere import Sphere
 from sympy import symbols
 
-x,y,z = symbols("x y z")
+x, y, z = symbols("x y z")
+
 
 def test_format_equation1():
     sphere1 = Sphere(eq=4*x**2 + 4*y**2 - 16*x - 24*y + 51)
     sphere2 = Sphere(eq=z**2 + (x - 2)**2 + (y - 3)**2 - 1/4)
     assert sphere1 == sphere2
 
+
 def test_format_equation2():
+    sphere1 = Sphere(eq=100*x**2+100*y**2-100*x+240*y-56)
     pass
+
 
 def test_format_equation3():
     pass
+
 
 def test_format_equation4():
     pass
