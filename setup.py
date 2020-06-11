7de174724f1f8e93f86a74cd806f7fca2a183e67
AoWangDrexel <aw3338@drexel.edu>
2020-06-11 00:59:41 -0400

    Add setup.py file

---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='axiomathbf',
+      version='0.1',
+      description='Mutlivariate Calculus solvers',
+      packages=['axiomathbf'],
+      zip_safe=False)
