2ee143eda91f57e5f177e6e990fac89a34824c42
AoWangDrexel <aw3338@drexel.edu>
2020-06-11 23:27:38 -0400

    Add magic methods to Matrix method

--- axiomathbf/Matrix.py
+++ axiomathbf/Matrix.py
@@ -5,23 +5,79 @@ class Matrix():
     def __init__(self, i=0, j=0, k=0):
         self.matrix = [i, j, k]
 
+    def __str__(self):
+        return str(self.getMatrix())
+
+    def __add__(self, other):
+        if isinstance(other, int) or isinstance(other, float):
+            m = [unit+other for unit in self.matrix]
+        if isinstance(other, Matrix):
+            m = [self.matrix[idx]+other[idx] for idx in range(3)]
+        return Matrix(m[0], m[1], m[2])
+
+    def __sub__(self, other):
+        if isinstance(other, int) or isinstance(other, float):
+            m = [unit-other for unit in self.matrix]
+        if isinstance(other, Matrix):
+            m = [self.matrix[idx]-other[idx] for idx in range(3)]
+        return Matrix(m[0], m[1], m[2])
+
+    def __floordiv__(self, other):
+        if isinstance(other, int) or isinstance(other, float):
+            m = [unit//other for unit in self.matrix]
+        if isinstance(other, Matrix):
+            m = [self.matrix[idx]//other[idx] for idx in range(3)]
+        return Matrix(m[0], m[1], m[2])
+
+    def __truediv__(self, other):
+        if isinstance(other, int) or isinstance(other, float):
+            m = [unit/other for unit in self.matrix]
+        if isinstance(other, Matrix):
+            m = [self.matrix[idx]/other[idx] for idx in range(3)]
+        return Matrix(m[0], m[1], m[2])
+
+    def __mul__(self, other):
+        if isinstance(other, int) or isinstance(other, float):
+            m = [unit*other for unit in self.matrix]
+        if isinstance(other, Matrix):
+            m = [self.matrix[idx]*other[idx] for idx in range(3)]
+        return Matrix(m[0], m[1], m[2])
+    
+    def __mod__(self, other):
+        if isinstance(other, int):
+            m = [unit%other for unit in self.matrix]
+        if isinstance(other, Matrix):
+            m = [self.matrix[idx]%other[idx] for idx in range(3)]
+        return Matrix(m[0], m[1], m[2])
+
+    def __getitem__(self, index):
+        return self.matrix[index]
+
+    def __len__(self):
+        return len(self.matrix)
+
     def dot(self, other):
         return sum([self.matrix[idx]*other[idx] for idx in range(3)])
 
     def cross(self, other):
-        i = self.matrix[1]*other[2] - self.matrix[2]*other[1]
-        j = -(self.matrix[0]*other[2] - self.matrix[2]*other[0])
-        k = self.matrix[0]*other[1] - self.matrix[1]*other[0]
+        i = self. _det(other, 1, 2)
+        j = -self. _det(other, 0, 2)
+        k = self. _det(other, 0, 1)
         return Matrix(i, j, k)
 
-    def _det(self):
-        pass
+    def _det(self, other, col1, col2):
+        sqrMatrix = self._section2by2(other, col1, col2)
+        return sqrMatrix[0][0]*sqrMatrix[1][1] - sqrMatrix[0][1]*sqrMatrix[1][0]
 
     def _vstack(self, other):
-        pass
+        return [self.matrix, other]
 
-    def _section2by2(self):
-        pass
+    def _section2by2(self, other, col1, col2):
+        twoByTwo = []
+        vstack = self._vstack(other)
+        for row in vstack:
+            twoByTwo.append([row[col1], row[col2]])
+        return twoByTwo
 
     def norm(self):
         return math.sqrt(sum([unit**2 for unit in self.matrix]))
@@ -35,3 +91,9 @@ class Matrix():
 
     def setMatrix(self, matrix):
         self.matrix = matrix
+
+
+if __name__ == "__main__":
+    u = Matrix(1, 2, 3)
+    v = Matrix(4, 5, 6)
+    print(u%2)
