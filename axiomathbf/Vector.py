0c016c7435cef5cdc4beae0cfdee4383e81fd077
AoWangDrexel <aw3338@drexel.edu>
2020-06-12 01:36:30 -0400

    Fix import for Vector module and inherit Matrix class

--- axiomathbf/Vector.py
+++ axiomathbf/Vector.py
@@ -1,86 +1,88 @@
-from sympy import *
 from sympy.calculus.util import continuous_domain
-from axiomathbf import Matrix
+from sympy import acos, Symbol, Interval, Intersection, diff, integrate, S, oo
+from axiomathbf.Matrix import Matrix
+
 
 class Vector(Matrix):
     """
 
     """
+
     def __init__(self, i=0, j=0, k=0):
-        self.vector = Matrix((i, j, k))
+        super().__init__(i, j, k)
         self.__t = Symbol('t')
 
     def __str__(self):
         """
 
         """
-        v = self.vector
+        v = self.matrix
         return '{}i + {}j + {}k'.format(v[0], v[1], v[2])
 
     def getAngle(self, other):
         """
 
         """
-        return acos(self.vector.dot(other) / (self.vector.norm() * other.norm()))
+        return acos(self.matrix.dot(other) / (self.matrix.norm() * other.norm()))
 
     def getDirCosine(self):
         """
 
         """
-        return [acos(unit/self.vector.norm()) for unit in self.vector]
+        return [acos(unit/self.matrix.norm()) for unit in self.matrix]
 
     def getProjection(self, other):
         """
 
         """
-        return (self.vector.dot(other)/(other.norm()**2))*other
+        return (self.matrix.dot(other)/(other.norm()**2))*other
 
     def getVector(self):
-        return self.vector
+        return self.matrix
 
-    def setVector(self, vector):
-        self.vector = vector
+    def setVector(self, matrix):
+        self.matrix = matrix
 
     def getCrossArea(self, other):
         """
 
         """
-        return self.vector.cross(other).norm()
+        return self.matrix.cross(other).norm()
 
     def getPPVolume(self, v, w):
         """
 
         """
-        return abs(self.vector.dot(v.cross(w)))
+        return abs(self.matrix.dot(v.cross(w)))
 
     def getPlane(self, point):
         """
 
         """
-        normVect = self.vector
+        normVect = self.matrix
         return normVect[0] * (self.x - point.x) + normVect[1] * (self.y - point.y) +\
             normVect[2] * (self.z - point.z)
 
-    def compareVector(self, other):
+    def comparematrix(self, other):
         """
 
 
         """
         result = ""
-        if self.vector.dot(other) == 0:
+        if self.matrix.dot(other) == 0:
             result = "Perpendicular"
-        elif self.vector.cross(other).norm() == 0:
+        elif self.matrix.cross(other).norm() == 0:
             result = "Parallel"
         else:
             result = "Skew"
-        return "{} and {}: {}".format(self.vector, other, result)
+        return "{} and {}: {}".format(self.matrix, other, result)
 
-    def getPointVectorLine(self, point):
+    def getPointmatrixLine(self, point):
         """
 
         """
         x, y, z = point
-        v1, v2, v3 = self.vector
+        v1, v2, v3 = self.matrix
         return '<x,y,z> = <{},{},{}> + <{},{},{}>t'.format(x, y, z, v1, v2, v3)
 
     def getDomainOfVectFunc(self):
@@ -90,7 +92,7 @@ class Vector(Matrix):
         domain = Interval(-oo, oo)
         domainOfFunctions = []
         t = self.__t
-        for function in self.vector:
+        for function in self.matrix:
             domainOfFunctions.append(continuous_domain(function, t, S.Reals))
             domain = Intersection(continuous_domain(
                 function, t, S.Reals), domain)
@@ -100,13 +102,13 @@ class Vector(Matrix):
         """
 
         """
-        return Matrix([diff(function, self.__t) for function in self.vector])
+        return Matrix([diff(function, self.__t) for function in self.matrix])
 
     def integrate(self):
         """
 
         """
-        return Matrix([integrate(function, self.__t) for function in self.vector])
+        return Matrix([integrate(function, self.__t) for function in self.matrix])
 
     def getTangentLine(self, point, t):
         """
@@ -114,5 +116,9 @@ class Vector(Matrix):
         """
         pass
 
+
 if __name__ == "__main__":
-    pass
+    u = Vector(0, 2, 3)
+    v = Vector(3, 4, 2)
+    print(u)
+    print(u.dot(v))
