0e8a58ddf0fd2d8cec0667c2109a43188b2e3f30
AoWangDrexel <aw3338@drexel.edu>
2020-06-12 23:43:47 -0400

    Refactor magic methods in Matrix

--- axiomathbf/Matrix.py
+++ axiomathbf/Matrix.py
@@ -1,61 +1,45 @@
 import math
-from sympy import simplify, diff, latex, sqrt
-import sympy
+from sympy import simplify, diff, latex, sqrt, Rational
 from IPython.display import display, Math
 
 
-class Matrix():
+class MatrixBase():
     def __init__(self, i=0, j=0, k=0):
         self.matrix = [i, j, k]
 
     def __str__(self):
-        return str(self.getMatrix())
+        return str(self.getMatrixBase())
 
     def _repr_pretty_(self, p, cycle):
         i, j, k = self.matrix
         return p.text(self.__str__()) if cycle else display(Math('\\left[\\begin{matrix}' + '{}\\\\{}\\\\{}\\end'.format(i, j, k) + '{matrix}\\right]'))
 
+    def _dunderHelper(self, other, operator):
+        if isinstance(other, MatrixBase):
+            m = eval('[{0}[idx]{1}{2}[idx] for idx in range(3)]'.format(
+                self.matrix, operator, other))
+        if isinstance(other, (int, float)):
+            m = eval('[unit{0}{1} for unit in self.matrix]'.format(
+                operator, other))
+        return MatrixBase(m[0], m[1], m[2])
+
     def __add__(self, other):
-        if isinstance(other, int) or isinstance(other, float):
-            m = [unit+other for unit in self.matrix]
-        if isinstance(other, Matrix):
-            m = [self.matrix[idx]+other[idx] for idx in range(3)]
-        return Matrix(m[0], m[1], m[2])
+        return self._dunderHelper(other, '+')
 
     def __sub__(self, other):
-        if isinstance(other, int) or isinstance(other, float):
-            m = [unit-other for unit in self.matrix]
-        if isinstance(other, Matrix):
-            m = [self.matrix[idx]-other[idx] for idx in range(3)]
-        return Matrix(m[0], m[1], m[2])
+        return self._dunderHelper(other, '-')
 
     def __floordiv__(self, other):
-        if isinstance(other, int) or isinstance(other, float):
-            m = [unit//other for unit in self.matrix]
-        if isinstance(other, Matrix):
-            m = [self.matrix[idx]//other[idx] for idx in range(3)]
-        return Matrix(m[0], m[1], m[2])
+        return self._dunderHelper(other, '//')
 
     def __truediv__(self, other):
-        if isinstance(other, int) or isinstance(other, float):
-            m = [unit/other for unit in self.matrix]
-        if isinstance(other, Matrix):
-            m = [self.matrix[idx]/other[idx] for idx in range(3)]
-        return Matrix(m[0], m[1], m[2])
+        return self._dunderHelper(other, '/')
 
     def __mul__(self, other):
-        if isinstance(other, Matrix):
-            m = [self.matrix[idx]*other[idx] for idx in range(3)]
-        else:
-            m = [unit*other for unit in self.matrix]
-        return Matrix(m[0], m[1], m[2])
+        return self._dunderHelper(other, '*')
 
     def __mod__(self, other):
-        if isinstance(other, int):
-            m = [unit % other for unit in self.matrix]
-        if isinstance(other, Matrix):
-            m = [self.matrix[idx] % other[idx] for idx in range(3)]
-        return Matrix(m[0], m[1], m[2])
+        return self._dunderHelper(other, '%')
 
     def __getitem__(self, index):
         return self.matrix[index]
@@ -70,11 +54,11 @@ class Matrix():
         i = self._det(other, 1, 2)
         j = -self._det(other, 0, 2)
         k = self._det(other, 0, 1)
-        return Matrix(i, j, k)
+        return MatrixBase(i, j, k)
 
     def _det(self, other, col1, col2):
-        sqrMatrix = self._section2by2(other, col1, col2)
-        return sqrMatrix[0][0]*sqrMatrix[1][1] - sqrMatrix[0][1]*sqrMatrix[1][0]
+        sqrMatrixBase = self._section2by2(other, col1, col2)
+        return sqrMatrixBase[0][0]*sqrMatrixBase[1][1] - sqrMatrixBase[0][1]*sqrMatrixBase[1][0]
 
     def _vstack(self, other):
         return [self.matrix, other]
@@ -87,7 +71,7 @@ class Matrix():
 
     def normalize(self):
         normalized = [unit/self.norm() for unit in self.matrix]
-        return Matrix(normalized[0], normalized[1], normalized[2])
+        return MatrixBase(normalized[0], normalized[1], normalized[2])
 
     def getJacobian(self, respect):
         matrix = []
@@ -96,16 +80,17 @@ class Matrix():
             for j in respect:
                 arr.append(diff(i, j))
                 matrix.append(arr)
-        return simplify(Matrix(matrix).det())
+        return simplify(MatrixBase(matrix).det())
 
-    def getMatrix(self):
+    def getMatrixBase(self):
         return self.matrix
 
-    def setMatrix(self, matrix):
+    def setMatrixBase(self, matrix):
         self.matrix = matrix
 
 
 if __name__ == "__main__":
-    u = Matrix(1, 2, 3)
-    v = Matrix(4, 5, 6)
-    print(u.normalize())
+    u = MatrixBase(1, 2, 3)
+    v = MatrixBase(4, 5, 6)
+    u += 2
+    print(u+v)
