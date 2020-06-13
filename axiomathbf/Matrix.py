571f447a5b9557bd5f5ca60519b5f9305ad80497
AoWangDrexel <aw3338@drexel.edu>
2020-06-13 11:01:48 -0400

    Change name back to Matrix class

--- axiomathbf/Matrix.py
+++ axiomathbf/Matrix.py
@@ -1,27 +1,26 @@
 import math
 from sympy import simplify, diff, latex, sqrt, Rational
 from IPython.display import display, Math
-
-
-class MatrixBase():
+import os
+class Matrix():
     def __init__(self, i=0, j=0, k=0):
         self.matrix = [i, j, k]
 
     def __str__(self):
-        return str(self.getMatrixBase())
+        return str(self.getMatrix())
 
     def _repr_pretty_(self, p, cycle):
         i, j, k = self.matrix
         return p.text(self.__str__()) if cycle else display(Math('\\left[\\begin{matrix}' + '{}\\\\{}\\\\{}\\end'.format(i, j, k) + '{matrix}\\right]'))
 
     def _dunderHelper(self, other, operator):
-        if isinstance(other, MatrixBase):
+        if isinstance(other, Matrix):
             m = eval('[{0}[idx]{1}{2}[idx] for idx in range(3)]'.format(
                 self.matrix, operator, other))
         if isinstance(other, (int, float)):
             m = eval('[unit{0}{1} for unit in self.matrix]'.format(
                 operator, other))
-        return MatrixBase(m[0], m[1], m[2])
+        return Matrix(m[0], m[1], m[2])
 
     def __add__(self, other):
         return self._dunderHelper(other, '+')
@@ -54,11 +53,11 @@ class MatrixBase():
         i = self._det(other, 1, 2)
         j = -self._det(other, 0, 2)
         k = self._det(other, 0, 1)
-        return MatrixBase(i, j, k)
+        return Matrix(i, j, k)
 
     def _det(self, other, col1, col2):
-        sqrMatrixBase = self._section2by2(other, col1, col2)
-        return sqrMatrixBase[0][0]*sqrMatrixBase[1][1] - sqrMatrixBase[0][1]*sqrMatrixBase[1][0]
+        sqrMatrix = self._section2by2(other, col1, col2)
+        return sqrMatrix[0][0]*sqrMatrix[1][1] - sqrMatrix[0][1]*sqrMatrix[1][0]
 
     def _vstack(self, other):
         return [self.matrix, other]
@@ -71,7 +70,7 @@ class MatrixBase():
 
     def normalize(self):
         normalized = [unit/self.norm() for unit in self.matrix]
-        return MatrixBase(normalized[0], normalized[1], normalized[2])
+        return Matrix(normalized[0], normalized[1], normalized[2])
 
     def getJacobian(self, respect):
         matrix = []
@@ -80,17 +79,19 @@ class MatrixBase():
             for j in respect:
                 arr.append(diff(i, j))
                 matrix.append(arr)
-        return simplify(MatrixBase(matrix).det())
+        return simplify(Matrix(matrix).det())
 
-    def getMatrixBase(self):
+    def getMatrix(self):
         return self.matrix
 
-    def setMatrixBase(self, matrix):
+    def setMatrix(self, matrix):
         self.matrix = matrix
 
 
 if __name__ == "__main__":
-    u = MatrixBase(1, 2, 3)
-    v = MatrixBase(4, 5, 6)
+    print(os.getcwd())
+
+    u = Matrix(1, 2, 3)
+    v = Matrix(4, 5, 6)
     u += 2
     print(u+v)
--- axiomathbf/__init__.py
+++
@@ -1,4 +0,0 @@
-from .Sphere import Sphere
-from .Matrix import MatrixBase
-from .Vector import Vector
-from .MVFunction import MVFunction
