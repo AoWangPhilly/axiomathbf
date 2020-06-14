ca2f316abd45193b3042b1a341595929ae4335da
AoWangDrexel <aw3338@drexel.edu>
2020-06-14 13:51:46 -0400

    Create a Binary Search Tree class

---
+++ axiomathbf/BST.py
@@ -0,0 +1,13 @@
+class Node():
+    def __init__(self, data, next=next):
+        self.data = data
+        self.next = next
+    
+    def setData(self, data):
+        self.data = data
+    
+    def getData(self):
+        return self.data
+    
+class BST():
+    pass
