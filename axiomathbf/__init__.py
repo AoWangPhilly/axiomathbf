4c24b71fce791f5cb1813ac06cc88f5f68ab3ee8
AoWangDrexel <aw3338@drexel.edu>
2020-09-11 00:42:06 -0400

    Add in Gradient and DD to import

--- axiomathbf/__init__.py
+++ axiomathbf/__init__.py
@@ -1,4 +1,4 @@
 from .parametric_lines import ParametricLine
-from .MVFunction import MVFunction
+from .MVFunction import MVFunction, Gradient, DirectionalDerivative
 from .plane import Plane
 from .vector_value import VectorFunction
