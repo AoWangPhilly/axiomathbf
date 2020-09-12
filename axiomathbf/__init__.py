387c856e3591758c856ed9001749a4007947f776
AoWangDrexel <aw3338@drexel.edu>
2020-09-12 16:45:51 -0400

    Add import for chainrule module

--- axiomathbf/__init__.py
+++ axiomathbf/__init__.py
@@ -2,3 +2,5 @@ from .parametric_lines import ParametricLine
 from .MVFunction import MVFunction, Gradient, DirectionalDerivative
 from .plane import Plane
 from .vector_value import VectorFunction
+from .extrema import Extrema
+from .chainrule import ChainRule
