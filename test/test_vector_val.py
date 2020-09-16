9d695c3334314ff44eb8c369fc6600d861631768
AoWangDrexel <aw3338@drexel.edu>
2020-09-16 15:31:47 -0400

    Pass vector function class test

--- test/test_vector_val.py
+++ test/test_vector_val.py
@@ -1,35 +1,49 @@
-from axiomathbf import VectorFunction
+from axiomathbf import VectorFunction, ParametricLine
 import pytest
 from sympy import Symbol, sqrt, ln, E, cos, sin, tan, sec, Union, Interval, oo, Reals, log, pi
 from sympy.abc import t
 
 
 def test_str():
-    pass
+    r1 = VectorFunction([3*t, t**2, t**3+1])
+    r2 = VectorFunction([cos(t), sin(t), t])
+    r3 = VectorFunction([ln(t), 1, t**2])
+    sols = ['<3*t, t**2, t**3 + 1>',
+            '<cos(t), sin(t), t>',
+            '<log(t), 1, t**2>']
+    for r, sol in zip((r1, r2, r3), sols):
+        assert str(r) == sol
 
 
 def test_eq():
-    pass
+    r1 = VectorFunction([6*t, 1/t, 0])
+    r2 = VectorFunction([sin(t), t**3, 1-t])
+    r3 = VectorFunction([2*t*cos(t), t**2, t-1])
+    sols = [VectorFunction([6*t, 1/t, 0]),
+            VectorFunction([sin(t), t**3, 1-t]),
+            VectorFunction([2*t*cos(t), t**2, t-1])]
+    for r, sol in zip((r1, r2, r3), sols):
+        assert r == sol
 
 
 def test_diff():
     v1 = VectorFunction([3*t**2, ln(t), 4])
     v2 = VectorFunction([sin(t), t**3, 1-t])
-    answers = [VectorFunction([6*t, 1/t, 0]),
-               VectorFunction(2*t*cos(t), t**2, t-1)]
-    for v, a in zip((v1, v2), answers):
-        assert v.derive() == a
+    v3 = VectorFunction([cos(t), sin(t), t])
+    sols = [VectorFunction([6*t, 1/t, 0]),
+            VectorFunction([cos(t), 3*t**2, -1]),
+            VectorFunction([-sin(t), cos(t), 1])]
+    for v, s in zip((v1, v2, v3), sols):
+        assert v.derive() == s
 
 
 def test_integration():
     v1 = VectorFunction([(2*t+1)**5, -1/t, 0])
     v2 = VectorFunction([sin(t), cos(t), tan(t)])
-    v3 = VectorFunction([E**t, E**(2*t), 0])
-    answers = [VectorFunction([(1/12)*(2*t+1)**6, - ln(abs(t)), 0]),
-               VectorFunction([-cos(t), sin(t), ln(abs(sec(t)))]),
-               VectorFunction([2, 4, 0])]
-    for v, a in zip((v1, v2, v3), answers):
-        pass
+    answers = [VectorFunction([16*t**6/3 + 16*t**5 + 20*t**4 + 40*t**3/3 + 5*t**2 + t, -log(t), 0]),
+               VectorFunction([-cos(t), sin(t), -ln(cos(t))])]
+    for v, a in zip((v1, v2), answers):
+        assert v.integrate() == a
 
 
 def test_domain():
@@ -45,18 +59,31 @@ def test_domain():
                Union(Interval.open(-oo, 0), Interval.Lopen(0, 4))]
 
     for v, d in zip((v1, v2, v3, v4, v5), answers):
-        assert v.getDomain() == d
+        assert v.get_domain() == d
 
 
 def test_plugin():
-    pass
+    r1, t1 = VectorFunction([-sin(t), cos(t), 1]), pi/2
+    r2, t2 = VectorFunction([6*t, 1/t, 0]), 2
+    r3, t3 = VectorFunction([cos(t), sin(t), t]), pi/2
+    sols = [[-1, 0, 1], [12, 1/2, 0], [0, 1, pi/2]]
+    for r, sol in zip(((r1, t1), (r2, t2), (r3, t3)), sols):
+        assert r[0].plugin(r[1]) == sol
 
 
 def test_line_tangent():
     r1, p1 = VectorFunction([ln(t), 2*sqrt(t), t**2]), [0, 2, 1]
     r2, p2 = VectorFunction([sin(t), cos(t), tan(t)]), pi
-    assert r1.getTangentLine(point=p1) == r2.getTangentLine(tau=p2)
+    l1 = ParametricLine(point=[0, 2, 1], vector=[1, 1, 2])
+    l2 = ParametricLine(point=[0, -1, 0], vector=[-1, 0, 1])
+    assert r1.get_tangent_line(point=p1) == l1
+    assert r2.get_tangent_line(tau=p2) == l2
 
 
 def test_solve_integration():
-    pass
+    r1, i1, p1 = VectorFunction([E**-t, 3*t**2, 0]), [2, -8, 0], 0
+    r2, i2, p2 = VectorFunction([4*t, E**t, 0]), [2, 3, 0], 0
+    sols = [VectorFunction([-E**(-t)+3, t**3-8, 0]),
+            VectorFunction([2*t**2+2, E**t+2, 0])]
+    assert r1.solve_integration(initial=i1, point=p1) == sols[0]
+    assert r2.solve_integration(initial=i2, point=p2) == sols[1]
