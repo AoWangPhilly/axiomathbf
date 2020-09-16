377aeb815a5e943883c6501d6c82e38a21a182bf
AoWangDrexel <aw3338@drexel.edu>
2020-09-16 01:07:09 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,35 @@
-from setuptools import setup
-
-setup(name='axiomathbf',
-      version='0.3',
-      description='Mutlivariate Calculus solvers',
-      packages=['axiomathbf'],
-      zip_safe=False)
+from distutils.core import setup
+setup(
+    name='axiomathbf',         # How you named your package folder (MyLib)
+    packages=['axiomathbf'],   # Chose the same as "name"
+    version='0.1',      # Start with a small number and increase it with every change you make
+    # Chose a license from here: https://help.github.com/articles/licensing-a-repository
+    license='MIT',
+    # Give a short description about your library
+    description='Multivariable Calculus Aid',
+    author='Ao Wang',                   # Type in your name
+    author_email='aw3338@drexel.edu',      # Type in your E-Mail
+    # Provide either the link to your github or to your website
+    url='https://github.com/AoWangPhilly',
+    # I explain this later on
+    download_url='https://github.com/user/reponame/archive/v_01.tar.gz',
+    # Keywords that define your package best
+    keywords=['Python', 'Multivariable Calculus', 'Sympy', 'Latex'],
+    install_requires=[            # I get to this in a second
+        'validators',
+        'beautifulsoup4',
+    ],
+    classifiers=[
+        # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+        'Development Status :: 3 - Alpha',
+        # Define that your audience are developers
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',   # Again, pick a license
+        # Specify which pyhton versions that you want to support
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+)
