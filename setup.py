1014db8d230c769662611ff3cfcbf08ef9bb0173
AoWangDrexel <aw3338@drexel.edu>
2020-09-18 23:25:21 -0400

    Use setuptools library instead

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
-from distutils.core import setup
+from setuptools import setup
 setup(
     name='axiomathbf',         # How you named your package folder (MyLib)
     packages=['axiomathbf'],   # Chose the same as "name"
-    version='0.1',      # Start with a small number and increase it with every change you make
+    version='0.0.1',      # Start with a small number and increase it with every change you make
     # Chose a license from here: https://help.github.com/articles/licensing-a-repository
     license='MIT',
     # Give a short description about your library
@@ -11,14 +11,8 @@ setup(
     author_email='aw3338@drexel.edu',      # Type in your E-Mail
     # Provide either the link to your github or to your website
     url='https://github.com/AoWangPhilly',
-    # I explain this later on
-    download_url='https://github.com/user/reponame/archive/v_01.tar.gz',
     # Keywords that define your package best
     keywords=['Python', 'Multivariable Calculus', 'Sympy', 'Latex'],
-    install_requires=[            # I get to this in a second
-        'validators',
-        'beautifulsoup4',
-    ],
     classifiers=[
         # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
         'Development Status :: 3 - Alpha',
